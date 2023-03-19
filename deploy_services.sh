ebbd4cd6fe36904a214ea6e3824f9459fc339efb
Mor Paz <mor@getport.io>
2023-03-19 14:16:44 +0200

    Updates to env

--- deploy_services.sh
+++ deploy_services.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+kubectl apply -f https://raw.githubusercontent.com/port-live-webinar/exporter-config/main/namespaces/namespaces.yml
+kubectl apply -f https://raw.githubusercontent.com/port-live-webinar/exporter-config/main/products/products.yml
 kubectl apply -f https://raw.githubusercontent.com/port-live-webinar/exporter-config/main/payment/payment.yml
 kubectl apply -f https://raw.githubusercontent.com/port-live-webinar/exporter-config/main/orders/orders.yml
--- deploy_update.sh
+++ deploy_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CLUSTER_NAME="webinar-cluster" # defaults to 'my-cluster'
+export CLUSTER_NAME="webinar"
 export PORT_CLIENT_ID="2aJdsPIDdceBhAsCj5Msnt5fa8kv1edm"
 export PORT_CLIENT_SECRET="hWbuMnZS0MZ1VLh20vxBOgyqBn8pMonABbxtfY2h6tdXw6qcMkD41yazfOcYJGoR"
 export CONFIG_YAML_URL=https://raw.githubusercontent.com/port-live-webinar/exporter-config/main/config.yaml
