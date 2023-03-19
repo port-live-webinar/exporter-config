32aeb68b51c86c3d5dc7041fa52bd4204343fd34
Mor Paz <mor@getport.io>
2023-03-19 12:52:29 +0200

    Updates

---
+++ deploy_original.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export CLUSTER_NAME="webinar-cluster" # defaults to 'my-cluster'
+export PORT_CLIENT_ID="2aJdsPIDdceBhAsCj5Msnt5fa8kv1edm"
+export PORT_CLIENT_SECRET="hWbuMnZS0MZ1VLh20vxBOgyqBn8pMonABbxtfY2h6tdXw6qcMkD41yazfOcYJGoR"
+curl -s https://raw.githubusercontent.com/port-labs/template-assets/main/kubernetes/install.sh | bash
